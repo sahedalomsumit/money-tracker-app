3ca3e8148632eb7f3db9cd585ba885a84d2a1197
sahedalomsumit <sahedalomsumit@gmail.com>
2026-09-19 13:04:30 +0300

    feat: initialize Android application project with Gradle wrapper, Compose, and Firebase dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
-    id("com.android.application") version "8.8.2" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.10" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.1.10" apply false
+    id("com.android.application") version "9.4.1" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.4.20" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
 }
